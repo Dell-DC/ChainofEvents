1f1c4c032453b7387a3139560de29a6f437fe96d
Aaron Cheung <21007144+AaronCheung430@users.noreply.github.com>
2024-11-03 12:23:09 +0000

    added camera movement

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const canvas = document.querySelector(".webgl")
 const scene = new THREE.Scene()
+let prevTime = performance.now();
+let moveForward = false, moveBackward = false, moveLeft = false, moveRight = false;
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+const rotation = new THREE.Vector2();
 
 const loader = new GLTFLoader()
-loader.load('demoscene.glb', function(glb){
+loader.load('the_great_sorcerers_room.glb', function(glb){
     console.log(glb)
     const root = glb.scene;
     scene.add(root);
@@ -38,7 +43,7 @@ const sizes = {
 }
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
-camera.position.set(-2,3,2)
+camera.position.set(1,3,5)
 scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer({
@@ -49,11 +54,74 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.shadowMap.enabled = true
 renderer.gammaOutput = true
-renderer.render(scene, camera)
 
 function animate(){
-    requestAnimationFrame(animate)
+    requestAnimationFrame(animate);
+
+    const time = performance.now();
+    const delta = (time - prevTime) / 1000;
+
+    velocity.x -= velocity.x * 10.0 * delta;
+    velocity.z -= velocity.z * 10.0 * delta;
+
+    if (moveForward) velocity.z -= 400.0 * delta;
+    if (moveBackward) velocity.z += 400.0 * delta;
+    if (moveLeft) velocity.x -= 400.0 * delta;
+    if (moveRight) velocity.x += 400.0 * delta;
+
+    camera.translateX(velocity.x * delta);
+    camera.translateZ(velocity.z * delta);
+
+    document.addEventListener('keydown', onKeyDown, false);
+    document.addEventListener('keyup', onKeyUp, false);
+
+    prevTime = time;
+
     renderer.render(scene, camera)
+
 }
+
+function onKeyDown(event) {
+    switch (event.code) {
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = true;
+            break;
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = true;
+            break;
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = true;
+            break;
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = true;
+            break;
+    }
+}
+
+function onKeyUp(event) {
+    switch (event.code) {
+        case 'ArrowUp':
+        case 'KeyW':
+            moveForward = false;
+            break;
+        case 'ArrowLeft':
+        case 'KeyA':
+            moveLeft = false;
+            break;
+        case 'ArrowDown':
+        case 'KeyS':
+            moveBackward = false;
+            break;
+        case 'ArrowRight':
+        case 'KeyD':
+            moveRight = false;
+            break;
+    }
+}
+
 
 animate()
