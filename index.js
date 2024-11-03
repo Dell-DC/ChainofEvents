8a9da69543953b421cbdcf9732bf08e473a7a17e
Aaron Cheung <21007144+AaronCheung430@users.noreply.github.com>
2024-11-03 12:07:29 +0000

    added 3d model

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-import * as THREE from './three.js-master/three.js-master/build/three.module.js';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const canvas = document.querySelector(".webgl")
 const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00
+const loader = new GLTFLoader()
+loader.load('demoscene.glb', function(glb){
+    console.log(glb)
+    const root = glb.scene;
+    scene.add(root);
+}, function(xhr){
+    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
+}, function(error){
+    console.log("An error occurred")
 })
 
-const boxMesh = new THREE.Mesh(geometry,material)
-scene.add(boxMesh)
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(-2,2,5)
+scene.add(light)
+
+const lights = new THREE.DirectionalLight(0xffffff, 1)
+lights.position.set(-2,2,-5)
+scene.add(lights)
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({
+//     color: 0x00ff00
+// })
+
+// const boxMesh = new THREE.Mesh(geometry,material)
+// scene.add(boxMesh)
 
 const sizes = {
     width: window.innerWidth,
@@ -17,7 +38,7 @@ const sizes = {
 }
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
-camera.position.set(0,1,2)
+camera.position.set(-2,3,2)
 scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer({
@@ -29,3 +50,10 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.shadowMap.enabled = true
 renderer.gammaOutput = true
 renderer.render(scene, camera)
+
+function animate(){
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+}
+
+animate()
